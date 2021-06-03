dc78b9b9017fcfa475113da29eb006c811d797e7
BrCdS <brunocristianocontato@gmail.com>
2021-06-02 22:49:26 -0300

    coment exemple

--- aglom/urls.py
+++ aglom/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
-from app_denuncias.views import ExampleView
+#from app_denuncias.views import ExampleView
 from app_auth.api.viewsets import RegisterView
 from rest_framework import routers
 from app_denuncias.api.viewsets import DenunciaViewSet
@@ -13,10 +13,12 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('token/', obtain_jwt_token),
 
-    path('example/', ExampleView.as_view()),
-
+    path('sign-in/', obtain_jwt_token),
     path('sign-up/', RegisterView.as_view(), name='auth_register'),
-
 ]
+
+'''
+    rota protegida, teste
+    path('example/', ExampleView.as_view()),
+'''
