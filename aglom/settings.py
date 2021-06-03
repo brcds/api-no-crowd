ed70a052277804f2f606ce7e644b2cb595a2f444
BrCdS <brunocristianocontato@gmail.com>
2021-06-02 22:43:27 -0300

    liberando READ sem authenticação

--- aglom/settings.py
+++ aglom/settings.py
@@ -71,7 +71,8 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
