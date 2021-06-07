4a4eb370843a9b5007b548da7e6f30792ab64659
BrCdS <brunocristianocontato@gmail.com>
2021-06-07 14:32:48 -0300

    configDEPLOY

--- aglom/settings.py
+++ aglom/settings.py
@@ -39,10 +39,21 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-'''CORS_ORIGIN_WHITELIST = ['http://localhost:*']
-CORS_ORIGIN_REGEX_WHITELIST = ['http://localhost:*']'''
 
+'''
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = ['http://localhost:*']
+CORS_ORIGIN_REGEX_WHITELIST = ['http://localhost:*']
+'''
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 ROOT_URLCONF = 'aglom.urls'
 
